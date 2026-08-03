c6de275158ff9fe715fdf7f3818b30d22dd81b53
crazyrabbit26 <panshulgupta26@gmail.com>
2026-08-04 00:03:14 +0530

    Stage 2: Implement student retrieval APIs

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from . import models,schemas
@@ -39,3 +39,39 @@ def create_student(
     db.refresh(new_student)
 
     return new_student
+
+
+@router.get(
+    "/students",
+    response_model=list[schemas.StudentResponse],
+    summary="Get all students"
+)
+def get_students(
+    db:Session=Depends(get_db)
+):
+    students=db.query(models.Student).all()
+
+    return students
+
+
+@router.get(
+    "/students/{student_id}",
+    response_model=schemas.StudentResponse,
+    summary="Get student by ID"
+)
+def get_student(
+    student_id:int,
+    db:Session=Depends(get_db)
+):
+    student=db.query(models.Student).filter(
+        models.Student.id==student_id
+    ).first()
+
+    if student is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+    return student
+
